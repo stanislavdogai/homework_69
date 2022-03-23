e75bbf38b881538357c6d22e708ba51fed731901
stanislavdogai <stanislavdogai01@gmail.com>
2022-03-24 04:53:52 +0600

    В методе add добавил в JsonResponse, status=400 и получаю ошибку в JS консоли, но не могу поймать текст самой ошибки что бы вывести её на странице

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,8 +1,7 @@
-from datetime import datetime
 import json
 
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse, HttpResponseBadRequest
+from django.http import HttpResponseNotAllowed, JsonResponse
 from django.shortcuts import render
 
 @ensure_csrf_cookie
@@ -12,50 +11,40 @@ def get_token_view(request, *args, **kwargs):
     return HttpResponseNotAllowed('Only GET request are allowed')
 
 
-
 def add(request):
-    answer = {}
-    if request.body:
-        data = json.loads(request.body)
-        print(data)
-        if data['num1'].isdigit() and data['num2'].isdigit():
-            total = int(data['num1']) + int(data['num2'])
-            return JsonResponse({'answer': total})
-        else:
-            return JsonResponse({'error': 'input only numbers'})
-    # if request.method == 'POST':
-    #     data = request.body
-    #     return JsonResponse({'answer':'qwe'})
+    data = json.loads(request.body)
+    if data['num1'].isdigit() and data['num2'].isdigit():
+        total = int(data['num1']) + int(data['num2'])
+        return JsonResponse({'answer': total, 'element' : '+'})
+    else:
+        return JsonResponse({'error': 'input only numbers'}, status=400)
 
 def subtract(request):
-    if request.body:
-        data = json.loads(request.body)
-        if data['num1'].isdigit() and data['num2'].isdigit():
-            total = int(data['num1']) - int(data['num2'])
-            return JsonResponse({'answer': total})
-        else:
-            return JsonResponse({'error': 'input only numbers'})
+    data = json.loads(request.body)
+    if data['num1'].isdigit() and data['num2'].isdigit():
+        total = int(data['num1']) - int(data['num2'])
+        print(total)
+        return JsonResponse({'answer': total, 'element' : '-'})
+    else:
+        return JsonResponse({'error': 'input only numbers'})
 
 def multiply(request):
-    answer = {}
     if request.body:
         data = json.loads(request.body)
-        print(data)
         if data['num1'].isdigit() and data['num2'].isdigit():
             total = int(data['num1']) * int(data['num2'])
-            return JsonResponse({'answer': total})
+            return JsonResponse({'answer': total, 'element' : '*'})
         else:
             return JsonResponse({'error': 'input only numbers'})
 
 def divide(request):
     data = json.loads(request.body)
-    print(data)
     if data['num1'].isdigit() and data['num2'].isdigit():
         if int(data['num1']) == 0 or int(data['num2']) == 0:
             return JsonResponse({'error': 'division by zero!'})
         else:
             total = int(data['num1']) / int(data['num2'])
-            return JsonResponse({'answer': total})
+            return JsonResponse({'answer': total, 'element' : '/'})
     else:
         return JsonResponse({'error': 'input only numbers'})
 
